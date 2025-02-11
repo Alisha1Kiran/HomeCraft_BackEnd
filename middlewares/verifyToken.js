7f293c8ac7064127fcfe9341cf1e3938ca32e924
Alisha1Kiran <61532157+Alisha1Kiran@users.noreply.github.com>
2025-02-11 23:07:38 +0400

    mv

--- middlewares/verifyToken.js
+++ middlewares/verifyToken.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 const verifyToken = (req, res, next) => {
-    console.log("Cookies received:", req.cookies);
+    console.log("Cookies received:", req);
     res.header("Access-Control-Allow-Origin", "https://homecraft-app.netlify.app"); // Allow frontend
     res.header("Access-Control-Allow-Credentials", "true"); // ✅ Allow cookies
     try {
