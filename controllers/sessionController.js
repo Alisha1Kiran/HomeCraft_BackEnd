fe1d20d4ed37960ce04dd49e308f24d87120b1f2
Alisha1Kiran <61532157+Alisha1Kiran@users.noreply.github.com>
2025-02-12 16:05:58 +0400

    reverted node_env setting production

--- controllers/sessionController.js
+++ controllers/sessionController.js
@@ -29,7 +29,7 @@ const userLogin = async (req, res) => {
     // Set the JWT token in an HTTP-only cookie
     res.cookie("authToken", token, {
       httpOnly: true, // Prevent access to cookies via JavaScript
-      secure: true, // Use secure cookies in production
+      secure: process.env.NODE_ENV === "production", // Use secure cookies in production
       maxAge: 60 * 60 * 1000, // Cookie expiry time (1 hour)
       sameSite: "None"
     });
@@ -55,7 +55,7 @@ const userLogout = async (req, res) => {
     // Clear the 'authToken' cookie
     res.clearCookie("authToken", {
       httpOnly: true, // Ensure the cookie is inaccessible via JavaScript
-      secure: true, // Use secure cookies in production
+      secure: process.env.NODE_ENV === "production", // Use secure cookies in production
       sameSite: "strict", // Protect against CSRF
     });
 
